b23412287b98e03022ac43c95b866302280e7f4e
Eliézer <111921317+eliezerdom@users.noreply.github.com>
2023-07-10 20:41:56 -0300

    Update jogo.py
    Documentation of index_tab & print_tab

--- jogo.py
+++ jogo.py
@@ -1,10 +1,34 @@
 def index_tab(tabuleiro, num):
+    '''
+    Parameters
+    ----------
+    tabuleiro : list
+        this the board.
+        
+    num : TYPE
+        board house index that you want.
+
+    Returns
+    -------
+    TYPE: (int, int)
+        index of num in tabuleiro.
+    '''
     for i in tabuleiro:
         if num in i:
             casa_num =  i.index(num)
             return (tabuleiro.index(i), casa_num)
         
 def print_tab(tabuleiro):
+    '''
+    Parameters
+    ----------
+    tabuleiro : list
+        it prints the game board.
+
+    Returns
+    -------
+    None.
+    '''
     for i in tabuleiro:
         print(*i, sep='|')
     print()
